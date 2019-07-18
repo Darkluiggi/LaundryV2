85fd6564925be1a01610e8ada8a13d64986fccf5
mavivasa <mavivasa@BE453S119C06ING.unal.loc>
2019-07-18 13:02:46 -0500

    fixed things in merge

--- src/Utils/TableUtils.java
+++ src/Utils/TableUtils.java
@@ -37,16 +37,16 @@ public class TableUtils {
     }
 
     public static void fillTableUser(JTable table, List<User> list) {
-        DefaultTableModel model = new DefaultTableModel(null, new String [] {
-        "Nombre", "Apellido", "Nombre de Usuario", "País", "Dirección", "Teléfono", "Contraseña", "Rol"
-    });
+        DefaultTableModel model = new DefaultTableModel(null, new String[]{
+            "Nombre", "Apellido", "Nombre de Usuario", "País", "Dirección", "Teléfono", "Contraseña", "Rol"
+        });
         for (User t : list) {
             model.addRow(new Object[]{t.getName(), t.getLastName(), t.getUserName(), t.getCountry(), t.getAdress(),
-            t.getPhone(),t.getPassword(),t.getRole()});
+                t.getPhone(), t.getPassword(), t.getRole()});
         }
         table.setModel(model);
-    } 
-    
+    }
+
     public static void fillTableArticle(JTable table, List<Article> list) {
         DefaultTableModel model = new DefaultTableModel(null, new String[]{
             "Género", "Nombre", "Lavado", "Lavado y Planchado", "Planchado", "Doblado"
@@ -60,51 +60,56 @@ public class TableUtils {
 
     public static void fillTableArticleRequest(JTable table, List<ArticleRequest> list) {
         DefaultTableModel model = new DefaultTableModel(null,
-    new String [] {
-        "Género", "Nombre", "Servicio", "Doblado", "Express", "Cantidad", "Subtotal"
-    }) {
-    Class[] types = new Class [] {
-        java.lang.Object.class, java.lang.Object.class, java.lang.Object.class, java.lang.Boolean.class, java.lang.Boolean.class, java.lang.Object.class, java.lang.Object.class
-    };
-    boolean[] canEdit = new boolean [] {
-        false, false, false, false, false, false, false
-    };
-    public Class getColumnClass(int columnIndex) {
-        return types [columnIndex];
-    }
+                new String[]{
+                    "#", "Género", "Nombre", "Servicio", "Doblado", "Express", "Cantidad", "Subtotal"
+                }) {
+            Class[] types = new Class[]{
+                java.lang.Object.class, java.lang.Object.class, java.lang.Object.class, java.lang.Object.class, java.lang.Boolean.class, java.lang.Boolean.class, java.lang.Object.class, java.lang.Object.class
+            };
+            boolean[] canEdit = new boolean[]{
+                false, false, false, false, false, false, false
+            };
 
-    public boolean isCellEditable(int rowIndex, int columnIndex) {
-        return canEdit [columnIndex];
-    }
-};
+            public Class getColumnClass(int columnIndex) {
+                return types[columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit[columnIndex];
+            }
+        };
         for (ArticleRequest t : list) {
             model.addRow(new Object[]{t.getId(), t.getArticle().getGender(), t.getArticle().getClothName(), t.getService(), t.isFold(), t.isExpress(), t.getQuantity(), t.getSubtotal()});
         }
         table.setModel(model);
     }
-    
+
     public static void fillTableArticleServices(JTable table, List<ArticleRequest> list) {
-        DefaultTableModel model = new DefaultTableModel(null, new String [] {
-        "ID", "Cabina", "Lavado", "Planchado", "Lav. y Plan.", "Express", "Total"
-    });
+        DefaultTableModel model = new DefaultTableModel(null, new String[]{
+            "ID", "Cabina", "Lavado", "Planchado", "Lav. y Plan.", "Express", "Total"
+        });
         for (ArticleRequest t : list) {
-        model.addRow(new Object[]{t.getRequest().getId(), t.getRequest().getCabin(), t.getArticle().getClothName(), 
-            t.getService(), t.isFold(), t.isExpress(), t.getQuantity(), t.getSubtotal()});
+            model.addRow(new Object[]{t.getRequest().getId(), t.getRequest().getCabin(), t.getArticle().getClothName(),
+                t.getService(), t.isFold(), t.isExpress(), t.getQuantity(), t.getSubtotal()});
         }
         table.setModel(model);
     }
-    
-    public class MiModelo extends DefaultTableModel
-{
-   /** Primera columna Boolean, segunda Integer y el resto Object */
-   public Class getColumnClass(int columna)
-   {
-      if (columna == 0) return Boolean.class;
-      if (columna == 1) return Integer.class;
-      return Object.class;
-   }
-}
-   
+
+    public class MiModelo extends DefaultTableModel {
+
+        /**
+         * Primera columna Boolean, segunda Integer y el resto Object
+         */
+        public Class getColumnClass(int columna) {
+            if (columna == 0) {
+                return Boolean.class;
+            }
+            if (columna == 1) {
+                return Integer.class;
+            }
+            return Object.class;
+        }
+    }
 
     public static void fillTableRequest(JTable table, List<Request> list) {
         DefaultTableModel model = new DefaultTableModel(null, new String[]{
@@ -127,33 +132,34 @@ public class TableUtils {
         popupMenu.add(editItem);
         table.setComponentPopupMenu(popupMenu);
     }
-    public static void guardaTabla(Cabin cabin,JTable table, Request request){
+
+    public static void guardaTabla(Cabin cabin, JTable table, Request request) {
         try {
             String company = ""
-                + "Lavandería a Bordo\n"
-                + "600 NW 1st Ave, Miami, FL‎\n"
-                + "Land: 812254630 Mob: 712205220 Fax: 812254639\n"
-                + " \n";
+                    + "Lavandería a Bordo\n"
+                    + "600 NW 1st Ave, Miami, FL‎\n"
+                    + "Land: 812254630 Mob: 712205220 Fax: 812254639\n"
+                    + " \n";
             String sucursalesCSVFile = "archivos/DatosTabla.txt";
-            BufferedWriter bfw = new BufferedWriter(new FileWriter(sucursalesCSVFile ));
+            BufferedWriter bfw = new BufferedWriter(new FileWriter(sucursalesCSVFile));
             bfw.write(company);
             bfw.write("Fecha:  ");
             bfw.write(request.getCreated_at().toString() + " \n ");
             bfw.write("Solicitud Nº: " + cabin.getId() + "\n");
-            bfw.write("Cliente: " );
+            bfw.write("Cliente: ");
 //          bfw.write(cabin.getResponsibleUser().getName());
             bfw.write("\n_________________________________________");
-            bfw.newLine(); 
-            for (int i = 0 ; i < table.getRowCount(); i++) //realiza un barrido por filas.
+            bfw.newLine();
+            for (int i = 0; i < table.getRowCount(); i++) //realiza un barrido por filas.
             {
-                for(int j = 0 ; j < table.getColumnCount();j++) //realiza un barrido por columnas.
-                { 
-                    bfw.write((table.getValueAt(i,j)).toString());
-                    if (j < table.getColumnCount() -1) { //agrega separador "," si no es el ultimo elemento de la fila.
+                for (int j = 0; j < table.getColumnCount(); j++) //realiza un barrido por columnas.
+                {
+                    bfw.write((table.getValueAt(i, j)).toString());
+                    if (j < table.getColumnCount() - 1) { //agrega separador "," si no es el ultimo elemento de la fila.
                         bfw.write("|");
                     }
                 }
-               
+
                 bfw.write("\n_________________________________________");
                 bfw.newLine();//inserta nueva linea.
             }
@@ -164,6 +170,7 @@ public class TableUtils {
             System.out.println("ERROR: Ocurrio un problema al salvar el archivo!" + e.getMessage());
         }
     }
+
     public static void clearTable(JTable table) {
         DefaultTableModel dm = (DefaultTableModel) table.getModel();
         int rowCount = dm.getRowCount();
@@ -185,7 +192,7 @@ public class TableUtils {
             ironSum = listAR.stream().filter(ar -> ar.getService().equals("Planchado")).mapToDouble(ar -> ar.getSubtotal()).sum();
             total = listAR.stream().mapToDouble(ar -> ar.getSubtotal()).sum();
             model.addRow(new Object[]{r.getId(), r.getCabin().getId(), washSum, ironSum, ironWashSum, total});
-}
+        }
         table.setModel(model);
     }
 
